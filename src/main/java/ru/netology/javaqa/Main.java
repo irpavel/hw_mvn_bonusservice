59d356c46194f732eded16e4b3ea3d2f8ef05045
Pavel Irinarkhov <irpavel@yandex.ru>
2023-05-27 01:05:43 +0600

    initial commit

---
+++ src/main/java/ru/netology/javaqa/Main.java
@@ -0,0 +1,15 @@
+package ru.netology.javaqa;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+//        long amount = 5_000;
+//        boolean registered = true;
+//        long expected = 150;
+//
+//        long actual = service.calculate(amount, registered);
+//
+//        System.out.println(expected + " == > == " + actual);
+    }
+}
